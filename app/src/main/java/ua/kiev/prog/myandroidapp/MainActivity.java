73e6faaed147671277c448196fc9d2fd0aacb4b8
Yevhenii Slyvinskyi <y.slyvinskyi@gmail.com>
2017-07-04 00:00:54 +0300

    updated buttons

--- app/src/main/java/ua/kiev/prog/myandroidapp/MainActivity.java
+++ app/src/main/java/ua/kiev/prog/myandroidapp/MainActivity.java
@@ -6,27 +6,51 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.TextView;
+import android.widget.Button;
+import android.content.Intent;
+import android.widget.GridView;
+import android.widget.ArrayAdapter;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        TextView view = (TextView) findViewById(R.id.textView);
-//        view.setText("123");
-//
-//        ViewGroup viewGroup = (ViewGroup) view.getParent();
-//        viewGroup.addView(view);
-//
-//        TextView textView = new TextView(this);
-//
-//        RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT,
-//                RelativeLayout.LayoutParams.WRAP_CONTENT);
-//        layoutParams.topMargin = 100;
-//        textView.setLayoutParams(layoutParams);
-//
-//        setContentView(textView);
+        final Button button1 = (Button) findViewById(R.id.button1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent dialIntent = new Intent("android.intent.action.DIAL");
+
+                /** Starting the Dialer activity */
+                startActivity(dialIntent);
+
+            }
+        });
+
+        final Button button3 = (Button) findViewById(R.id.button3);
+        button3.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent smsIntent = new Intent(Intent.ACTION_VIEW);
+                smsIntent.setType("vnd.android-dir/mms-sms");
+                startActivity(smsIntent);
+
+            }
+        });
+
+        final Button button2 = (Button) findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                setContentView(R.layout.second_activity);
+
+
+            }
+        });
+
+
     }
+
 }
